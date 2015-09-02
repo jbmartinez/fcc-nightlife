968a24fcadfd509a6c9f3e637fdadb6196b541f5
Juan Martínez <jbmartinezs@yahoo.com>
2015-09-02 20:04:28 +0000

    change to 4sq API

--- server/api/search/search.controller.js
+++ server/api/search/search.controller.js
@@ -2,25 +2,45 @@
 
 var _ = require('lodash');
 var Search = require('./search.model');
-var yelp = require("yelp").createClient({
-  consumer_key: "consumer-key", 
-  consumer_secret: "consumer-secret",
-  token: "token",
-  token_secret: "token-secret"
-});
+var config = {
+  'secrets' : {
+    'clientId': process.env.FOURSQ_ID,
+    'clientSecret': process.env.FOURSQ_SECRET,
+    'redirectUrl': process.env.FOURSQ_REDIRECT
+  }
+};
 
-// See http://www.yelp.com/developers/documentation/v2/search_api
-yelp.search({term: "food", location: "Montreal"}, function(error, data) {
-  console.log(error);
-  console.log(data);
-});
+var foursquare = require('node-foursquare')(config);
 
 // Get list of searchs
+// exports.index = function(req, res) {
+//   Search.find(function (err, searchs) {
+//     if(err) { return handleError(res, err); }
+//     return res.status(200).json(searchs);
+//   });
+// };
 exports.index = function(req, res) {
-  Search.find(function (err, searchs) {
+  // Search.find(function (err, searchs) {
+  var params = {section: 'coffee', venuePhotos: 1};
+  foursquare.Venues.explore(null, null, 'New York, NY', params, null, function(err, searchs) {
     if(err) { return handleError(res, err); }
-    return res.status(200).json(searchs);
+    var results = searchs.groups[0].items
+      .map(function(item) {
+        return {
+          id: item.venue.id,
+          name: item.venue.name,
+          rating: item.venue.rating,
+          location: item.venue.location.formattedAddress[0],
+          link: 'https://foursquare.com/v/' + item.venue.id,
+          photo: item.venue.featuredPhotos.items[0].prefix + '100x100' + item.venue.featuredPhotos.items[0].suffix
+        };
+      });
+
+    console.log(JSON.stringify(results));
+    return res.status(200).json(results);
+    // return res.status(200).json(searchs);
   });
+  // });
 };
 
 // Get a single search
